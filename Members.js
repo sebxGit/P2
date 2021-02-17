1de8a7a634e5a4003496867ff7688f9717984978
sebxGit <72699667+sebxGit@users.noreply.github.com>
2021-02-17 09:09:07 +0100

    upd 2
    a

--- Members.js
+++ Members.js
@@ -1,11 +1,11 @@
-const members = [
+members = [
   {id: 1,
   name: 'Sebx',
   room_id: 'abc'
   },
   {id: 2,
-    name: 'Danijula',
-    room_id: 'abc'
+  name: 'Danijula',
+  room_id: 'abc'
   }
 ]
 
--- index.js
+++ index.js
@@ -1,32 +1,22 @@
 const express = require('express');
-const path =require('path');
+const path = require('path');
 const logger = require('./middleware/logger');
-const members = require('./Members');
-
-// https://youtu.be/L72fhGm1tfE?t=2244
 
 const app = express();
 
+//https://youtu.be/L72fhGm1tfE?t=2700
 // init middleware. Creates log in terminal with everything done.
-app.use(logger); // 
+app.use(logger); 
 
-// Gets all members
-app.get('/api/members', (req, res) => res.json(members)); 
-
-// Get Single Member
-app.get('/api/members/:id', (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if(found){
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  } else{
-    res.status(400).json({msg: `No member with the id of ${req.params.id}`});
-  }
-});
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api/members', require('./routes/api/members'));
+
 const PORT = process.env.PORT || 5000; // Get initial port from environment variable or 5000. 
 
-app.listen(PORT, () => console.log('Server started on port ' + PORT)); 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
